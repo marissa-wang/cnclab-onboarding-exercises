3c20e09ba288996552bd8759c757563a75f3e670
Marissa Wang <marisw2@uci.edu>
2025-10-16 22:46:50 -0700

    added word

--- utils/check_keys.py
+++ utils/check_keys.py
@@ -1,6 +1,6 @@
 from scipy.io import loadmat
 
-# This was used really on in the code haha, doesn't have much use now, but I will
+# This was used really early on in the code haha, doesn't have much use now, but I will
 # keep it here for archive purposes
 
 filename = '../circle-noise-data/aggregate/grating2AFC S11.mat'
